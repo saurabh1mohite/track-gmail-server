b9c0ba43ec411628de47287cf3f5cdddc6c0d833
saurabh1mohite <mohitesaurabh650@gmail.com>
2023-04-06 15:33:03 -0700

    Update code

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
 from flask import Flask, send_file, request, jsonify, make_response
 from flask_sqlalchemy import SQLAlchemy
 import os
-from flask import Flask
-from flask_cors import CORS
+from flask_cors import CORS, cross_origin
 import logging
 import sys
 
@@ -10,17 +9,13 @@ app = Flask(__name__)
 app.logger.setLevel(logging.INFO)
 app.logger.addHandler(logging.StreamHandler(sys.stdout))
 
-CORS(app, origins = ['https://mail.google.com' ])
-
+CORS(app)
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 app.config['SQLALCHEMY_DATABASE_URI'] ='sqlite:///' + os.path.join(basedir, 'imagekey.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-
-
 db = SQLAlchemy(app)
-
 
 class ImageKey(db.Model):
     image_key = db.Column(db.Integer, primary_key=True)
@@ -29,13 +24,12 @@ class ImageKey(db.Model):
     def __repr__(self):
         return f'Image key - {self.image_key}; Visits - {self.visits}'
 
-
-
 @app.route('/')
 def helloWorld():
     return 'Hello world!'
 
 @app.route('/images', methods=['POST'])
+@cross_origin(origin='https://mail.google.com', headers=['Content-Type', 'Authorization'])
 def add_new_image():
     data = request.json
     if not data or not data.get('image_key') or not isinstance(data.get('image_key'), int):
@@ -47,11 +41,10 @@ def add_new_image():
     image = ImageKey(image_key = data['image_key'], visits = 0)
     db.session.add(image)
     db.session.commit()
-    response = make_response(jsonify({'success' : 'Image Index added successfully'}), 201)
-    response.headers['Access-Control-Allow-Origin'] = '*'
-    return response
+    return jsonify({'success' : 'Image Index added successfully'}), 201
 
 @app.route('/images/<int:image_key>')
+@cross_origin(origin='https://mail.google.com', headers=['Content-Type', 'Authorization'])
 def download_image(image_key):
     """
     Download image from the specified path and serve it to the user
@@ -71,10 +64,7 @@ def download_image(image_key):
     else:
         app.logger.info("No key found")
     # Serve file to the user
-    response = make_response(send_file('images/tree.jpg', as_attachment=True))
-    response.headers['Access-Control-Allow-Origin'] = '*'
-    return send_file('images/tree.jpg', as_attachment=True)       
-
+    return send_file('images/tree.jpg', as_attachment=True)
 
 if __name__ == '__main__':
     app.run(debug = True)
